32347f78758ba133c7a0c65206de43769997df96
Inu <i0n4h0o7@gmail.com>
2025-01-03 18:26:48 +0900

    추가

--- lib/repository/todos_repository.dart
+++ lib/repository/todos_repository.dart
@@ -2,6 +2,11 @@ import 'package:todolist_bloc/data_provider/api_provider.dart';
 import 'package:todolist_bloc/model/todos.dart';
 
 class TodosRepository {
+  /// Repository (Data handler)
+  ///
+  /// Data Provider로부터 데이터를 받아
+  /// 필터링 등 변형하여 BLOC에게 전달
+
   final TodosApiProvider _todosApiProvider = TodosApiProvider();
 
   Future<Todos> fetchAllTodos() async => _todosApiProvider.fetchTodosList();
